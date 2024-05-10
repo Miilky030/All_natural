4c83fbf27fed88fb4dcc5583eb8e87da4f9252f9
Melanie2901 <125890034+Melanie2901@users.noreply.github.com>
2024-05-10 12:13:32 -0400

    cambios

--- admin_update_profile.php
+++ admin_update_profile.php
@@ -77,7 +77,7 @@ if(isset($_POST['update_profile'])){
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
 
    <!-- custom css file link  -->
-   <link rel="stylesheet" href="css/components.css">
+   <link rel="stylesheet" href="proyect/css/components.css">
 
 </head>
 <body>
